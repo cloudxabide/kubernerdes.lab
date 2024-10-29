bf182069ac63663e334a4f121050b446484a5770
James Radtke <cloudxabide@gmail.com>
2024-10-28 21:08:30 -0400

    generic update

--- Scripts/50_Deploy_Docker_EKS-A_Cluster.sh
+++ Scripts/50_Deploy_Docker_EKS-A_Cluster.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-
 docker kill $(docker ps -a | egrep 'boots|eks' | awk '{ print $1 }' | grep -v CONTAINER)
 docker rm $(docker ps -a | egrep 'boots|eks' | awk '{ print $1 }' | grep -v CONTAINER)
 
 CLUSTER_NAME=mgmt
+mkdir -p ~/eksa/$CLUSTER_NAME; cd $_
 # eksctl anywhere generate clusterconfig $CLUSTER_NAME --provider docker > $CLUSTER_NAME.yaml
 eksctl anywhere create cluster -f $CLUSTER_NAME.yaml
 
 export KUBECONFIG=$(find $PWD -name $CLUSTER_NAME.kind.kubeconfig)
 
-kubectl get pods -A | awk '{ print "kubectl logs --tail 5 " $2 " -n " $1  "; echo; echo" }' | sh - | more
+kubectl get pods -A | awk '{ print "kubectl logs --tail 3 " $2 " -n " $1  "; echo; echo; echo" }' | sh - | more
+for POD in $(docker ps -a | grep eks-anywhere | awk '{ print $1 }'); do docker logs --tail 5 $POD; echo "######################"; done
 
 
