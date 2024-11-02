1417a2426260eede659c0e86365b3f1c8b2a3fa8
James Radtke <cloudxabide@gmail.com>
2024-11-02 16:10:23 -0400

    generic update

--- Scripts/50_Deploy_Docker_EKS-A_Cluster.sh
+++ Scripts/50_Deploy_Docker_EKS-A_Cluster.sh
@@ -5,22 +5,43 @@ docker ps -a || { echo "there is an issue with Docker.  Is it running?  Exiting 
 docker kill $(docker ps -a | egrep 'boots|eks' | awk '{ print $1 }' | grep -v CONTAINER)
 docker rm $(docker ps -a | egrep 'boots|eks' | awk '{ print $1 }' | grep -v CONTAINER)
 
-DADATE=$(date +%F)
 CLUSTER_NAME=mgmt
+
+# Create the Directories to utilize
+DADATE=$(date +%F)
 mkdir -p ~/eksa/$CLUSTER_NAME/; cd $_
+[ -d latest ] && rm latest
 mkdir ${DADATE};
 ln -s ${DADATE} latest; cd $_
 
 
-# eksctl anywhere generate clusterconfig $CLUSTER_NAME --provider docker > $CLUSTER_NAME.yaml
+eksctl anywhere generate clusterconfig $CLUSTER_NAME --provider docker > $CLUSTER_NAME.yaml
+MyTweaks() {
+cp $CLUSTER_NAME.yaml $CLUSTER_NAME.orig
 sed -i -e 's/192.168.0.0/172.16.0.0/g' $CLUSTER_NAME.yaml
+yq -i e --front-matter=process '.spec.controlPlaneConfiguration.count |= 3'  $CLUSTER_NAME.yaml
+yq -i e --front-matter=process '.spec.externalEtcdConfiguration.count |= 3'  $CLUSTER_NAME.yaml
+yq -i e --front-matter=process '.spec.workerNodeGroupConfigurations.count |= 2'  $CLUSTER_NAME.yaml
+sdiff $CLUSTER_NAME.yaml $CLUSTER_NAME.orig
+}
+
 unset KUBECONFIG
 eksctl anywhere create cluster -f $CLUSTER_NAME.yaml
 
+# To access K8s during the install
 export KUBECONFIG=$(find $PWD/ -name $CLUSTER_NAME.kind.kubeconfig)
-export KUBECONFIG=$(find $PWD/  | grep "${CLUSTER_NAME}.eks-a-cluster.kubeconfig")
-find $PWD/ -name $CLUSTER_NAME.eks-a-cluster.kubeconfig)
 
+# Access K8s post install
+export KUBECONFIG=$(find $PWD/  | grep "${CLUSTER_NAME}.eks-a-cluster.kubeconfig")
+
+# Some quick troubleshooting tips
 kubectl get pods -A | awk '{ print "kubectl logs --tail 3 " $2 " -n " $1  "; echo; echo; echo" }' | sh - | more
 for POD in $(docker ps -a | grep eks-anywhere | awk '{ print $1 }'); do docker logs --tail 5 $POD; echo "######################"; done
+
+exit 0
+
+
+#yq e --front-matter=process '.site_name |= "src/" + sub("[^a-zA-Z]", "")' file.yaml
+yq e --front-matter=process '.spec.controlPlaneConfiguration.count |= "3")  $CLUSTER_NAME.yaml
+/" + sub("[^a-zA-Z]", "")' file.yaml
 
